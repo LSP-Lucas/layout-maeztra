2de0de26bf6f000ae86ff976774217c1015f06e8
Lucas Pedroso <lucassppedroso@gmail.com>
2020-12-31 18:53:02 -0300

    criação do layout com bootstrap 5

--- public/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-const responsiveCarousel = document.querySelector(".glide");
-
-const config = {
-  type: 'slider',
-  perView: 1,
-  focusAt: 'center'
-}
-
-new Glide(responsiveCarousel, config).mount()
