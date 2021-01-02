ec0f587e1d74fd8db1c48e094093cf766909db8b
Lucas Pedroso <lucassppedroso@gmail.com>
2021-01-02 17:30:02 -0300

    correão do banner

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -9,4 +9,13 @@ $(function(){
     nextArrow: $("#arrow-next")
   });
 
+  $(".banner-web-mobile").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: $("#arrow-prev-mobile"),
+    nextArrow: $("#arrow-next-mobile")
+  });
+
 })
