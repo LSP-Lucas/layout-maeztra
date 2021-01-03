5c67ee5c1dcd32e5803ac2c2dfbc5b635ebb528e
Lucas Pedroso <lucassppedroso@gmail.com>
2021-01-03 19:39:59 -0300

    finalização dos sliders

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -98,4 +98,50 @@ $(function(){
     ]
   });
 
+  $(".slide-most-requesteds").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    centerMode: false,
+    prevArrow: $("#arrow-prev-most"),
+    nextArrow: $("#arrow-next-most"),
+    responsive: [
+      {
+        breakpoint: 1632,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 1180,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 790,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          arrows: false,
+        }
+      },
+    ]
+  });
+
 })
